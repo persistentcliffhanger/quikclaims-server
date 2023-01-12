397e6c40fd0215613436e40b4e9d93560b573c72
r0bc0ulter <rcoulter05@qub.ac.uk>
2023-01-12 13:04:12 +0000

    jdhjd

---
+++ src/main/java/com/allstate/quikclaimsserver/QuikclaimsServerApplication.java
@@ -0,0 +1,13 @@
+package com.allstate.quikclaimsserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuikclaimsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuikclaimsServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/allstate/quikclaimsserver/QuikclaimsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allstate.quikclaimsserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuikclaimsServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
